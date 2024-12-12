66f7a502a55494916dfcec3d8946c775b84f8336
Sharone j.p wonmally <sharonejpwonmally@gmail.com>
2024-12-12 12:15:51 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -5,9 +5,21 @@ public class Nomor1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
+       printFizzBuzz(n);
+    }
 
-        /* Pekerjaan anda berakhir sini */
+    public static void printFizzBuzz(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
